8c7c893166dce06ea65bf87a9e3fb528c5bb99d9
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-09-20 13:54:58 +0800

    add user-interface

---
+++ app/data.ts
@@ -0,0 +1,184 @@
+export const data = [
+  {
+    title: "FULLSTACK DEVELOPER (Node + Vue) | Work from home*",
+    company: "FlexisourceIT",
+    location: "Makati City",
+    image: "/company-logo.png"
+  },
+  {
+    title:
+      "Fullstack Developer (Nodejs | Reactjs | Vuejs | Typescript | Javascript)",
+    company: "YONDU INC.",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "BACK END DEVELOPER (NODE JS) --- TEMP WFH",
+    company: "Information Professionals, Inc.",
+    location: "Taguig City",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Backend NodeJS Engineer",
+    company: "Complete Development (CoDev)",
+    location: "Cebu (Cebu City)",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Full Stack Developer (Java and JavaScript)",
+    company: "Sysgen RPO, Inc.",
+    location: "Taguig City",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title:
+      "Back End Developer (NodeJS and TypeScript) | Permanent WFH | Day Shift",
+    company: "Emapta",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Full stack Developer(.NET core, NodeJS, Angular)",
+    company: "Encora Management Services Philippines Inc.",
+    location: "Makati City",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Software Engineer/Developer (Hybrid)",
+    company: "YONDU INC.",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.JS Developer",
+    company: "Collabera Technologies Private Limited Inc.",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Full Stack Developer (Java & Javascript)",
+    company: "Sysgen RPO, Inc.",
+    location: "Taguig City",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Senior Full Stack Node Developer",
+    company: "Cloudstaff Philippines Inc.",
+    location: "Pampanga (Others)",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Fullstack Developer (Hybrid Setup)",
+    company: "YONDU INC.",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Senior Node.JS Developer",
+    company: "Collabera Technologies Private Limited Inc.",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Senior Developer (Java, PL/SQL, Javascript, NodeJS)",
+    company: "IT Managers Inc.",
+    location: "Taguig City",
+    image: "/company-logo.png"
+  },
+  {
+    title: "IT Developer - WFH",
+    company: "SHORE REMOTE OUTSOURCE INC.",
+    location: "Pampanga (Others)",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Senior Developer",
+    company: "Next BPO Solutions, Inc.",
+    location: "Davao del Sur (Davao City)",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.JS Developer",
+    company: "Pointwest",
+    location: "Quezon City",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Sr back end developer",
+    company: "Nityo Infotech Services Philippines Inc.",
+    location: "Muntinlupa City",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node.js Developer | Temporary WFH* | Up to 80,000 Joining Bonus!*",
+    company: "Accenture",
+    location: "National Capital Reg",
+    image: "/company-logo.png"
+  },
+  {
+    title: "Node JS Developer",
+    company: "KMC Solutions",
+    location: "Taguig City",
+    image: "/company-logo.png"
+  },
+]
